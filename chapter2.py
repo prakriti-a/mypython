c0309b49a3621ce1d474207cb4ccd26d41a54dd4
=prakritibro <=>
2019-02-26 10:02:59 +0530

    commit

---
+++ chapter2.py
@@ -0,0 +1,18 @@
+
+#exercise 2
+username=input('Enter your name:')
+print("hello"+username)
+
+
+#exercise 3
+hours=input('Enter hours:')
+rate=input('Enter rate:')
+pay=float(hours)*float(rate)
+print('Gross pay is:'+pay)
+
+
+#exercise 5
+temp_celcius=input('Enter temperature in celcius:')
+temp_farenheit=(float(temp_celcius)*1.8)+32
+print('Temperature in °F is:'+ temp_farenheit)
+
