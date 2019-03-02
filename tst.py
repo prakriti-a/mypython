5c1ef860bc5a5a56e4fcbbd689bff589aa5de071
=prakritibro <=>
2019-03-02 10:56:19 +0530

    update

--- tst.py
+++ tst.py
@@ -1,2 +1,13 @@
 print('hi')
+s='example'
+i=356
+f=72.72883
+print(s)
+print(i)
+print(f)
+print(type(s))
+print(type(i))
+print(type(f))
+print('type of:',s,'is',type(s))
+print('type of:',f,'is',type(f))
 
